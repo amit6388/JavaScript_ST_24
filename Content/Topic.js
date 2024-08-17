d6bc4e8a6baa8b94c69599caec8698917c920464
amit6388 <amitpoly2020@gmail.com>
2024-08-17 09:30:32 +0530

    day 5

---
+++ Content/Topic.js
@@ -0,0 +1,32 @@
+/*
+################
+Topics Covered
+################
+
+1. Intro 
+2. History
+3. alert 
+4. prompt box 
+5. document.write 
+6. console.log
+7. Data Types
+8. var, let, const
+9. Type Casting: parseInt, parseFloat, etc.
+10. Functions-:
+11. Callback Functions
+12. setInterval()
+13. setTimeout()
+14. clearInterval()
+15. Conditional Statements
+16. Events
+
+   ############ DOM Manipulation ##########
+17. document.getElementById(" ")
+18. document.querySelector("p")    
+19. innerHTML// 
+20. innerText
+    - Element (input, img, p, div)
+
+   ############ Upcoming ##########
+21. this keyword, etc.
+*/
